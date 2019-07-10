52b3115a34cc2ccf891016bd676b5a3eab3eacc3
in4system <support@in4system.com>
2019-07-10 10:33:42 +0200

    Updates

---
+++ precache-manifest.877bbffbe3c357d9b911f917fee87101.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "323017352398cf7aa31f2166235e4fa6",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "28a20b0a363448acc83b",
+    "url": "/contactmanager/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "b59b01dd016254440a4b",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "28a20b0a363448acc83b",
+    "url": "/contactmanager/static/js/2.292b09d7.chunk.js"
+  },
+  {
+    "revision": "b59b01dd016254440a4b",
+    "url": "/contactmanager/static/js/main.ac8c079e.chunk.js"
+  },
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  }
+]);
